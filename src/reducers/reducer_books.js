5432d789266838dcdcf80430aa22d6289040feca
CR <rajshekhar@protonmail.com>
2017-12-31 05:09:57 -0600

    rednering data by defining redux function and connecting to react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+
+    return [
+        { title:'Javascript: The good parts'},
+        { title:'harry potter'},
+        { title:'Dark knight'},
+        { title:'Rudby'},
+    ]
+}
